57befb052f0d03d662f7ae8b8350b224798b1e68
Vishal Chauhan <vishalchauhan@Vishals-MacBook-Air.local>
2023-04-22 19:49:22 +0530

    webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -39,14 +39,3 @@ module.exports = {
 }
 
 
-//Loaders are transformations that are applied to the source code of a module. They allow you to pre-process files as you import or “load” them. Thus, loaders are kind of like “tasks” in other build tools and provide a powerful way to handle front-end build steps
-
-//Plugins work at bundle or chunk level and usually work at the end of the bundle generation process. Plugins can also modify how the bundles themselves are created.
-
-// clean:true
-
-//This plugin extracts CSS into separate files. It creates a CSS file per JS file which contains CSS
-
-//simplifies creation of HTML files to serve your webpack bundles.
-
-//this plugin will remove all files inside webpack's output.path directory, as well as all unused webpack assets after every successful rebuild.
